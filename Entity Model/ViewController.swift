333739dabf041d546b3c7c4a3f70c8fb31b967ef
Aditya Patriazka <patriazka@gmail.com>
2019-05-15 13:21:12 +0700

    Initial Commit

---
+++ Entity Model/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Entity Model
+//
+//  Created by Aditya Patriazka on 15/05/19.
+//  Copyright © 2019 Aditya Patriazka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
